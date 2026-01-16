d6ede28bbdf890db26017f0ca7b1fe4294598c49
JeonYouwe <jyouwe02@gmail.com>
2026-01-17 02:39:28 +0900

    syft, grype로 json파일 생성, 검사 이력 json 파일 생성, 3개 json 파일 데이터 가공한 코드 final_report.py 그리고 백엔드에 전송하는 코드가지 완료 (API주면 바로 전송 가능)

---
+++ youwe/Inpsection_report.py
@@ -0,0 +1,33 @@
+import json
+from datetime import datetime
+import os
+
+def create_scan_report(user_name):
+    # 1. 정보 설정 (딕셔너리로 작성)
+    report_data = {
+        "report_info": {
+            "project_name": "SBOM-security-project",
+            "inspector": user_name,  # 검사하는 사람 이름
+            "scan_date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"), # 실시간 시간+날짜
+            "environment": "Windows / Git Bash"
+        },
+        "files_generated": [
+            "libraries.json",
+            "vulnerabilities.json"
+        ]
+    }
+
+    # 2. 'inspection_info' 폴더가 없으면 생성
+    if not os.path.exists('inspection_info'):
+        os.makedirs('inspection_info')
+
+    # 3. JSON 파일로 저장 (덮어쓰기)
+    file_path = "inspection_info/scan_info.json"
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(report_data, f, indent=4, ensure_ascii=False)
+
+    print(f"✅ 검사 정보 파일이 생성완료!: {file_path}")
+
+# 실행
+if __name__ == "__main__":
+    create_scan_report("JeonYouwe") # 본인 이름을 넣으세요

efd32740c42004bd76990e416daf1a901feb0f18
Ahnjungyeo <h625062@gmail.com>
2026-01-30 20:29:15 +0900

    선모 화면 나오게 gwangyeon 설정 완

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // 1. 이거 추가
 import { AuthProvider } from "./auth/AuthContext";
 import App from "./App";
 import "./index.css";
@@ -9,8 +10,11 @@ if (!root) throw new Error("root element not found");
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
+    {/* 2. BrowserRouter가 가장 바깥에서 감싸줘야 useNavigate 에러가 사라집니다! */}
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );

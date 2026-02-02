a065e2bedce6d6d6617bf5b78f0294a97ce36c48
Ahnjungyeo <h625062@gmail.com>
2026-02-02 15:41:28 +0900

    feat: 이제 작업하면 됌

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Ahn_front 정상화 완료!</h1>
+      <p>이제 요새를 지을 준비가 되었습니다.</p>
+    </div>
+  );
+}
+
+export default App; // 이게 있어야 main.tsx에서 에러가 안 납니다!
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // 리더님이 남겨두신 그 App.tsx를 가져옵니다
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
